42a4adda33a4b91b189b2e0fa4ce711a809cc270
vinsennugraha <90520595+vinsennugraha@users.noreply.github.com>
2024-05-28 14:18:34 +0700

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -1,4 +1,4 @@
-var deadline = 'february 3 2024 18:40:18 GMT-1000';
+var deadline = 'juni 2 2024 11:00:18 GMT-1000';
 function time_remaining(endtime){
 	var t = Date.parse(endtime) - Date.parse(new Date());
 	var seconds = Math.floor( (t/1000) % 60 );
